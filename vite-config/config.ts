bdb7e62c073de51438b158eb140ce58ea492eff0
evan70 <ivanpaldan@yandex.com>
2024-06-10 23:20:14 +0200

    new file:   vite-config/config.ts 	new file:   vite-config/twig-html.ts

---
+++ vite-config/config.ts
@@ -0,0 +1,41 @@
+export default {
+    base: './',
+    server: {
+        open: '/layouts/index.html',
+        host: true
+    },
+    rootDir: 'src',
+    publicDir: 'static',
+    buildDir: 'static/build',
+    layoutsDir: 'layouts',
+    dataDir: 'data',
+    iconsDir: 'icons',
+    imageDir: 'img',
+
+    imagemin: {
+        webp: false,
+        optipng: {
+            optimizationLevel: 7
+        },
+        mozjpeg: {
+            quality: 75
+        },
+        pngquant: {
+            quality: [ 0.8, 0.9 ],
+            speed: 1,
+            floyd: 1
+        },
+        svgo: {
+            plugins: [
+                {
+                    name: 'removeViewBox',
+                    active: false
+                },
+                {
+                    name: 'removeEmptyAttrs',
+                    active: false
+                }
+            ]
+        }
+    }
+};
---
+++ vite-config/twig-html.ts
@@ -0,0 +1,166 @@
+import fastGlob from 'fast-glob';
+import fs from 'fs-extra';
+import merge from 'merge';
+import path from 'path';
+import pretty from 'pretty';
+import type { HmrContext, PluginOption, UserConfig, ViteDevServer } from 'vite';
+import twigPlugin from '@luckydye/vite-plugin-twig';
+
+const { globSync } = fastGlob;
+
+import config from './config';
+
+type Pages = { [entryAlias: string]: string };
+
+function getData(): any {
+    let data = {};
+
+    globSync(path.resolve(`${ config.rootDir }/${ config.dataDir }/**/*.json`)).forEach(filePath => {
+        const pathParse = path.parse(filePath);
+        const dataPath = path.resolve(`${ config.rootDir }/${ config.dataDir }`);
+        const foldersPath = (pathParse.dir + `/${ pathParse.name }`).replace(dataPath, '').replace(/^\//, '');
+        const folders = foldersPath.split('/');
+        const newData = folders.reverse().reduce((prev, current) => (
+            { [ current ]: { ...prev } }
+        ), JSON.parse(fs.readFileSync(filePath).toString()));
+
+        data = merge.recursive(data, newData);
+    });
+
+    return { data };
+}
+
+function getTwigHtml(twigFilePath: string): string {
+    return `<script type="application/json" data-template="${ twigFilePath }">{}</script>`;
+}
+
+function createPages(): Pages {
+    const pages: Pages = {};
+
+    globSync(path.resolve(`${ config.rootDir }/${ config.layoutsDir }/*.twig`)).forEach(filePath => {
+        const fileProps = path.parse(filePath);
+        const htmlPath = path.resolve(`${ config.rootDir }/${ config.layoutsDir }/${ fileProps.name }.html`);
+
+        fs.writeFileSync(
+            htmlPath,
+            getTwigHtml(fileProps.base)
+        );
+
+        pages[ fileProps.name ] = htmlPath;
+    });
+
+    return pages;
+}
+
+export default function twigHtmlPlugin(): PluginOption[] {
+    let pages: Pages;
+
+    return [
+        {
+            name: 'twig-html',
+            configureServer(server: ViteDevServer) {
+                return () => server.middlewares.use('/', async (req, res, next) => {
+                    if (!req.originalUrl || !req.url) {
+                        return next();
+                    }
+
+                    const pathProps = path.parse(req.originalUrl);
+                    const nameTwigPath = path.resolve(`${ config.rootDir }/${ config.layoutsDir }/${ pathProps.name }.twig`);
+                    const indexTwigPath = path.resolve(`${ config.rootDir }/${ config.layoutsDir }/index.twig`);
+                    const errorTwigPath = path.resolve(`${ config.rootDir }/${ config.layoutsDir }/404.twig`);
+
+                    if (fs.existsSync(nameTwigPath)) {
+                        const transformedHtml = await server.transformIndexHtml(req.url, getTwigHtml(`${ pathProps.name }.twig`), req.originalUrl);
+
+                        res.statusCode = 200;
+                        res.write(transformedHtml);
+
+                        return res.end();
+                    } else if (
+                        (
+                            (pathProps.dir === '/' && !pathProps.name) ||
+                            pathProps.name === config.layoutsDir ||
+                            pathProps.dir.includes(config.layoutsDir)
+                        ) && fs.existsSync(indexTwigPath)
+                    ) {
+                        const transformedHtml = await server.transformIndexHtml(req.url, getTwigHtml('index.twig'), req.originalUrl);
+
+                        res.statusCode = 301;
+                        res.write(transformedHtml);
+
+                        return res.end();
+                    } else if (fs.existsSync(errorTwigPath)) {
+                        const transformedHtml = await server.transformIndexHtml(req.url, getTwigHtml('404.twig'), req.originalUrl);
+
+                        res.statusCode = 404;
+                        res.write(transformedHtml);
+
+                        return res.end();
+                    }
+
+                    return next();
+                });
+            },
+            transformIndexHtml: {
+                order: 'pre',
+                async handler(content: string) {
+                    if (!content.startsWith('<script type="application/json" data-template')) {
+                        return content;
+                    }
+
+                    return content.replace('{}', JSON.stringify(getData()));
+                }
+            },
+            handleHotUpdate({ file, server }: HmrContext) {
+                if (/.(json)$/.test(file)) {
+                    server.ws.send({ type: 'full-reload' });
+                }
+            },
+            config(config: UserConfig, { command }: { command: string }) {
+                if (command === 'build') {
+                    pages = createPages();
+
+                    config.build = {
+                        ...config.build,
+                        rollupOptions: {
+                            ...config.build?.rollupOptions,
+                            input: {
+                                ...(config.build?.rollupOptions?.input as object),
+                                ...pages
+                            }
+                        }
+                    };
+                }
+
+                return null;
+            },
+            closeBundle() {
+                for (const [ , filePath ] of Object.entries(pages)) {
+                    if (fs.existsSync(filePath)) {
+                        fs.rmSync(filePath, {
+                            recursive: true
+                        });
+                    }
+                }
+            }
+        },
+        twigPlugin({
+            settings: {
+                'views': `${ config.rootDir }/${ config.layoutsDir }`,
+                'twig options': false
+            }
+        }),
+        {
+            name: 'prettify-html',
+            apply: 'build',
+            transformIndexHtml: {
+                order: 'post',
+                async handler(content: string) {
+                    return pretty(content, {
+                        ocd: true
+                    });
+                }
+            }
+        }
+    ];
+}
