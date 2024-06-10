62cfc60fac93a6ba91332c95736daa0bb56a9b57
evan70 <ivanpaldan@yandex.com>
2024-06-10 23:19:52 +0200

    new file:   postcss.config.cjs

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+    plugins: [
+        require('postcss-advanced-variables'),
+        require('tailwindcss/nesting'),
+        require('tailwindcss'),
+        require('autoprefixer')({
+            remove: false
+        })
+    ]
+};
